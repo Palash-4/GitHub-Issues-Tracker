a1cfd527e61743bc010c475ac3ca900ac5a1803e
Palash-4 <hasanalltarek9658@gmail.com>
2026-03-08 05:41:20 +0600

    login successful and dashboard create

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("hi");
+// Login
+function login() {
+    // console.log("login");
+    const username = document.getElementById("username").value.trim()
+    const password = document.getElementById("password").value.trim()
+
+    if (username === "admin" && password === "admin123") {
+        window.location.href = "dashboard.html"
+    }
+    else {
+        alert("Invalid username or password")
+    }
+}
+
+
+
